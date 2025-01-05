82596e579362ead806ed332ca507a1d573881455
ABigWalrus <51272935+ABigWalrus@users.noreply.github.com>
2025-01-05 12:32:42 +0100

    Delete AGame directory

--- AGame/src/Game23_Scrolling/AvatarTexture.java
+++
@@ -1,9 +0,0 @@
-package Game23_Scrolling;
-
-import javax.swing.*;
-import java.awt.*;
-
-public final class AvatarTexture {
-    public static Image avatarDefault= new ImageIcon("AGame/src/Textures/Player/Wizard.png").getImage();
-
-}
--- AGame/src/Game23_Scrolling/MapBackground.java
+++
@@ -1,9 +0,0 @@
-package Game23_Scrolling;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-
-public class MapBackground {
-    public static Image mainBackground = new ImageIcon("AGame/src/Textures/Map/MainMap.png").getImage();
-}
