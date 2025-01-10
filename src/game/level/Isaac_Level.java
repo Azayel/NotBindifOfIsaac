bb93395b75d713e20464d2187fa852ef7c71f8b8
Marcel <non>
2025-01-10 13:34:36 +0100

    Maps Visuals added Boss+Normal

--- src/game/level/Isaac_Level.java
+++ src/game/level/Isaac_Level.java
@@ -29,7 +29,7 @@ public class Isaac_Level {
     //Create a Level
     public void CreateLevel(int maxRooms, int level) {
         //Init Starting Room and add to list
-        startingRoom = new Isaac_Room(Isaac_TextureRoom.mapDefault, 1920, 1080, Isaac_Sounds.StartingLevelMusic, Isaac_RoomType.NORMAL);
+        startingRoom = new Isaac_Room(Isaac_TextureRoom.mapDefault, 1920, 1080, Isaac_Sounds.StartingLevelMusic, Isaac_RoomType.START);
         rooms.add(startingRoom);
 
         while (rooms.size() < maxRooms - 1) {
@@ -41,22 +41,22 @@ public class Isaac_Level {
 
             //Direction
             if (randomDirection == 0 && currentRoom.topRoom == null) {
-                Isaac_Room newRoom = new Isaac_Room(Isaac_TextureRoom.mapDefault, 1920, 1080, Isaac_Sounds.MainMusic, Isaac_RoomType.NORMAL);
+                Isaac_Room newRoom = new Isaac_Room(Isaac_TextureRoom.mapNormal, 1920, 1080, Isaac_Sounds.MainMusic, Isaac_RoomType.NORMAL);
                 currentRoom.topRoom = newRoom;
                 newRoom.bottomRoom = currentRoom;
                 rooms.add(newRoom);
             } else if (randomDirection == 1 && currentRoom.rightRoom == null) {
-                Isaac_Room newRoom = new Isaac_Room(Isaac_TextureRoom.mapDefault, 1920, 1080, Isaac_Sounds.MainMusic, Isaac_RoomType.NORMAL);
+                Isaac_Room newRoom = new Isaac_Room(Isaac_TextureRoom.mapNormal, 1920, 1080, Isaac_Sounds.MainMusic, Isaac_RoomType.NORMAL);
                 currentRoom.rightRoom = newRoom;
                 newRoom.leftRoom = currentRoom;
                 rooms.add(newRoom);
             } else if (randomDirection == 2 && currentRoom.bottomRoom == null) {
-                Isaac_Room newRoom = new Isaac_Room(Isaac_TextureRoom.mapDefault, 1920, 1080, Isaac_Sounds.MainMusic, Isaac_RoomType.NORMAL);
+                Isaac_Room newRoom = new Isaac_Room(Isaac_TextureRoom.mapNormal, 1920, 1080, Isaac_Sounds.MainMusic, Isaac_RoomType.NORMAL);
                 currentRoom.bottomRoom = newRoom;
                 newRoom.topRoom = currentRoom;
                 rooms.add(newRoom);
             } else if (randomDirection == 3 && currentRoom.leftRoom == null) {
-                Isaac_Room newRoom = new Isaac_Room(Isaac_TextureRoom.mapDefault, 1920, 1080, Isaac_Sounds.MainMusic, Isaac_RoomType.NORMAL);
+                Isaac_Room newRoom = new Isaac_Room(Isaac_TextureRoom.mapNormal, 1920, 1080, Isaac_Sounds.MainMusic, Isaac_RoomType.NORMAL);
                 currentRoom.leftRoom = newRoom;
                 newRoom.rightRoom = currentRoom;
                 rooms.add(newRoom);
--- src/game/utils/Isaac_TextureRoom.java
+++ src/game/utils/Isaac_TextureRoom.java
@@ -10,12 +10,14 @@ public class Isaac_TextureRoom {
 
     public static BufferedImage mapDefault;
     public static BufferedImage mapBoss;
+    public static BufferedImage mapNormal;
 
     static {
         try {
             // Load the game map background image
             mapDefault = read(new File("src/assets/Maps/Isaac_GameBackground1.png"));
-            mapBoss = read(new File("src/assets/Maps/Isaac_GameBackground1.png"));
+            mapBoss = read(new File("src/assets/Maps/Isaac_GameBackground_BOSS.png"));
+            mapNormal = read(new File("src/assets/Maps/Isaac_GameBackground_NORMAL.png"));
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Error loading textures!");
